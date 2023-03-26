8613d855d63b1507b9768f9814f17ca24198ed4e
Derrick Brown <dbrownpv7@gmail.com>
2023-03-26 14:46:24 -0500

    Initial Commit

---
+++ StockTwoTabTest/StockTwoTabTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockTwoTabTestApp.swift
+//  StockTwoTabTest
+//
+//  Created by Derrick Brown II on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct StockTwoTabTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
